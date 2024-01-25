b2273d34b368483592cf6686c8ef6a6f4645163f
Invoker0416 <ChangJian0416@outlook.com>
2024-01-25 09:41:49 -0800

    first commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}

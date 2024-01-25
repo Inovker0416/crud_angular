f969232cfedfa3ad36285cf26560bc58cb8da6c9
Invoker0416 <ChangJian0416@outlook.com>
2024-01-25 10:37:02 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component'
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormComponent],
+  imports: [CommonModule, RouterOutlet, FormComponent,MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule, MatFormFieldModule, MatInputModule,MatDividerModule,MatButtonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
